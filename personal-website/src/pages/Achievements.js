4955d9c2b17f706c08fe6c45c3610abeefa85762
ajinkya0919 <bhaleraoajinkya878@gmail.com>
2024-05-27 04:33:12 +0200

    Achievements details

---
+++ personal-website/src/pages/Achievements.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Achievements.css';
+
+const Achievements = () => (
+  <div className="achievements">
+    <h2>Achievements</h2>
+    <ul>
+      <li>Student Ambassador – CSE @Mahindra University</li>
+      <li>Presented and successfully executed an agribusiness model at the E-Summit 2022</li>
+      <li>Captained university football team to champion two inter-college tournaments</li>
+      <li>Served as the organizing lead for Creative Literature Fest 2020 held for 200+ students in Pune</li>
+    </ul>
+  </div>
+);
+
+export default Achievements;
