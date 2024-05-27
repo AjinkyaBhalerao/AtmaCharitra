5107c027b11a5efb72fc74a9e72ed98f8adddead
ajinkya0919 <bhaleraoajinkya878@gmail.com>
2024-05-27 03:59:17 +0200

    Dashboard javascript file

---
+++ personal-website/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="home">
+    <div className="intro">
+      <img src="C:\Users\Dell\Desktop\Website\AtmaCharitra\profile.jpg" alt="Profile" className="profile-photo" />
+      <h1>Ajinkya Bhalerao</h1>
+      <p>Data Scientist with a passion for solving complex problems through data-driven solutions.</p>
+      <div className="scroll-indicator">
+        <span>↓</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
